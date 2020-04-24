3681f01462c2e4abdbbd14f95b1433dcb7b3ede8
MikanHako1024 <MikanHako@outlook.com>
2020-04-24 21:04:10 +0800

    完成行消除

--- Assets/Scripts/TetrisTilemap.cs
+++ Assets/Scripts/TetrisTilemap.cs
@@ -18,13 +18,14 @@ public class TetrisTilemap : MonoBehaviour {
 	private void FixedUpdate () {
 		UpdateAllShape();
 	}
-
-
+	
+	
+	public float CreateInterval = 1f;
 	[System.NonSerialized]
 	public float lastCreateTime = -60f;
 
 	private void Update() {
-		if (Input.GetKey(KeyCode.Z) && Time.time - lastCreateTime > 1f) {
+		if (Input.GetKey(KeyCode.Z) && Time.time - lastCreateTime > CreateInterval) {
 			TetrisShape shape = new TetrisShape();
 			shape.SetPosition(GetTopCenterPosition());
 			AddShape(shape);
@@ -34,8 +35,7 @@ public class TetrisTilemap : MonoBehaviour {
 			selectedShape = shape;
 		}
 	}
-
-
+	
 
 	#region 地图数据
 
@@ -70,12 +70,24 @@ public class TetrisTilemap : MonoBehaviour {
 
 	public int GetTilemapData(int x, int y) {
 		if (!(0 <= x && x < tilemapWidth)) return -1;
-		if (tilemapHeight <= y && y < tilemapHeightEx) return 0;
+		//if (tilemapHeight <= y && y < tilemapHeightEx) return 0;
 		if (!(0 <= y && y < tilemapHeight)) return -1;
 		return tilemapData[y][x];
 	}
 	public int GetTilemapData(Vector2Int pos) {
 		return GetTilemapData(pos.x, pos.y);
+	}
+	
+	// 对于高度拓展的部分 需要视为地图内
+	public int GetTilemapDataEx(int x, int y) {
+		//if (tilemapHeight <= y && y < tilemapHeightEx) return 0;
+		if (!(0 <= x && x < tilemapWidth)) return -1;
+		if (tilemapHeight <= y && y < tilemapHeightEx) return 0;
+		if (!(0 <= y && y < tilemapHeight)) return -1;
+		return GetTilemapData(x, y);
+	}
+	public int GetTilemapDataEx(Vector2Int pos) {
+		return GetTilemapDataEx(pos.x, pos.y);
 	}
 
 	public void SetTilemapData(int x, int y, int data) {
@@ -142,7 +154,8 @@ public class TetrisTilemap : MonoBehaviour {
 		Vector2Int[] list = shape.GetOccupyPositionList();
 		foreach (Vector2Int pos in list) {
 			//if (GetTilemapData(pos) > 0) {
-			if (GetTilemapData(pos) != 0) {
+			//if (GetTilemapData(pos) != 0) {
+			if (GetTilemapDataEx(pos) != 0) {
 				return false;
 			}
 		}
@@ -225,10 +238,12 @@ public class TetrisTilemap : MonoBehaviour {
 				//if (CheckShapeMoveVaild(shape, ShapeMoveType.down)) {
 				if (!CheckShapeStop(shape)) {
 					InvokeShapeMove(shape, ShapeMoveType.down);
-					if (CheckShapeStop(shape)) {
-						//StopShape(shape);
-						MarkStopShape(shape);
-					}
+					//if (CheckShapeStop(shape)) {
+					//	//StopShape(shape);
+					//	MarkStopShape(shape);
+					//}
+					// ？在下降之后不再立刻判断结束 ...
+					// ？这样可以允许在即将结束前再次移动 ...
 				}
 				else {
 					//StopShape(shape);
@@ -294,11 +309,11 @@ public class TetrisTilemap : MonoBehaviour {
 	
 	public TetrisShape selectedShape = null;
 
+	/*
 	public float keyInterval = 0.1f;
 	[System.NonSerialized]
 	public float lastKeyTime = -60f;
-	// ？...
-
+	
 	public bool allowKeyDown {
 		get {
 			return Time.time - lastKeyTime >= keyInterval;
@@ -327,14 +342,65 @@ public class TetrisTilemap : MonoBehaviour {
 			}
 		}
 	}
+	*/
+
+	// ？不是限制每次触发的间隔 ...
+	// ？而是应该把判定的Input.GetKey改成Input.GetKeyDown ...
+	
+	public float keyInterval = 0.1f;
+	[System.NonSerialized]
+	public float lastKeyTimeLR = -60f;
+	public float lastKeyTimeD = -60f;
+	
+	public bool allowKeyDownLR {
+		get { return Time.time - lastKeyTimeLR >= keyInterval; }
+	}
+	public bool allowKeyDownD {
+		get { return Time.time - lastKeyTimeD >= keyInterval; }
+	}
+	
+	public void RefreshKeyTimeLR() {
+		lastKeyTimeLR = Time.time;
+	}
+	public void RefreshKeyTimeD() {
+		lastKeyTimeD = Time.time;
+	}
+
+	// ？TODO : 自己写一个控制类 ...
+
+	public void UpdateShapeMove() {
+		if (selectedShape != null) {
+			if (Input.GetKey(KeyCode.LeftArrow) && allowKeyDownLR) {
+				InvokeShapeMoveIfCan(selectedShape, ShapeMoveType.left);
+				RefreshKeyTimeLR();
+			}
+			else if (Input.GetKey(KeyCode.RightArrow) && allowKeyDownLR) {
+				InvokeShapeMoveIfCan(selectedShape, ShapeMoveType.right);
+				RefreshKeyTimeLR();
+			}
+			else if (Input.GetKeyDown(KeyCode.UpArrow)) {
+				//InvokeShapeMoveIfCan(selectedShape, ShapeMoveType.spin);
+				InvokeShapeMoveIfCan(selectedShape, ShapeMoveType.spinInv);
+			}
+			else if (Input.GetKeyDown(KeyCode.DownArrow) && allowKeyDownD) {
+				InvokeShapeMoveIfCan(selectedShape, ShapeMoveType.down);
+				RefreshKeyTimeD();
+			}
+		}
+	}
 
 	#endregion
 
 
 	#region 更新显示
 
+	public TileBase GetTileBase(int data) {
+		return 0 <= data && data < tileList.Length ? tileList[data] : null;
+	}
+
+	// ？显示到tilemap上 允许超出tilemapData的范围 ...
 	public void ShowTile(int x, int y, int data) {
-		tilemap.SetTile(new Vector3Int(x, y, 0), tileList[data]);
+		tilemap.SetTile(new Vector3Int(x, y, 0), GetTileBase(data));
 	}
 	public void ShowTile(Vector2Int pos, int data) {
 		ShowTile(pos.x, pos.y, data);
@@ -348,7 +414,7 @@ public class TetrisTilemap : MonoBehaviour {
 			foreach (Vector2Int pos in list) {
 				//ShowTile(pos, 0);
 				data = GetTilemapData(pos);
-				data = data == -1 ? 0 : data;
+				//data = data == -1 ? 0 : data;
 				ShowTile(pos, data);
 			}
 		}
@@ -364,14 +430,6 @@ public class TetrisTilemap : MonoBehaviour {
 		}
 	}
 
-	public void UpdateAllShape() {
-		ClearShapeShow();
-		UpdateAllShapeDown();
-		UpdateMarkStopShape();
-		UpdateShapeMove();
-		ShowAllShape();
-	}
-
 	public void RefreshTilemapShow() {
 		for (int y = 0; y < tilemapHeight; y++) {
 			for (int x = 0; x < tilemapWidth; x++) {
@@ -380,13 +438,103 @@ public class TetrisTilemap : MonoBehaviour {
 		}
 		for (int y = tilemapHeight; y < tilemapHeightEx; y++) {
 			for (int x = 0; x < tilemapWidth; x++) {
-				ShowTile(x, y, 0);
+				//ShowTile(x, y, 0);
+				ShowTile(x, y, -1);
 			}
 		}
 		ShowAllShape();
 	}
 
 	#endregion
+
+
+	#region 判断和消除行
+
+	public void UpdateCheckEliminateLine() {
+		CheckEliminateAllLine();
+		// TODO : 增加分数 等
+	}
+
+	public int CheckEliminateAllLine() {
+		// 暂时直接判断所有行 而不是只判断变动的行
+		
+		//int[] yList = new int[tilemapHeight];
+		//int size = 0;
+		//for (int y = 0; y < tilemapHeight; y++) {
+		//	if (CheckLine(y)) {
+		//		yList[size++] = y;
+		//	}
+		//}
+		//EliminateLines(yList, size);
+		//return size;
+		List<int> list = new List<int>(); // TODO : 写成静态成员
+		for (int y = 0; y < tilemapHeight; y++) {
+			if (CheckLine(y)) {
+				list.Add(y);
+			}
+		}
+		EliminateLines(list);
+		return list.Count;
+		// 返回消除行数 用于在其他地方计算分数等
+	}
+
+	public bool CheckLine(int y) {
+		for (int x = 0; x < tilemapWidth; x++) {
+			if (GetTilemapData(x, y) == 0) { // 暂时不包括小于0的
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	//public void EliminateLines(int[] yList, int size) {
+	public void EliminateLines(List<int> list) {
+		if (list.Count <= 0) return ;
+
+		// TODO : 调用 播放音效和动画 等
+
+		int index = 0;
+		for (int y = 0; y < tilemapHeight; y++) {
+			if (!list.Contains(y)) {
+			// ？用判断是否存在list的方式 而不是循环list ...
+			// ？这样可以允许list无序 ...
+
+				// line[y] copy to line[index++] and clear line[y]
+
+				// ？这里暂时直接 替换数组引用 和 重新创建数组 ...
+				//tilemapData[index++] = tilemapData[y];
+				//tilemapData[y] = new int[tilemapWidth];
+				// ...
+				// ？这样会 ...
+				// ...
+				
+				for (int x = 0; x < tilemapWidth; x++) {
+					if (index != y) {
+						SetTilemapData(x, index, GetTilemapData(x, y));
+					}
+				}
+				index++;
+			}
+		}
+		// ？之后还要处理未处理的index ~ 结尾行
+		for (int y = index; y < tilemapHeight; y++) {
+			for (int x = 0; x < tilemapWidth; x++) {
+				SetTilemapData(x, y, 0);
+			}
+		}
+	}
+
+	#endregion
+
+
+	public void UpdateAllShape() {
+		ClearShapeShow();
+		UpdateAllShapeDown();
+		UpdateMarkStopShape();
+		UpdateShapeMove();
+		UpdateCheckEliminateLine();
+		ShowAllShape();
+	}
 }
 
 // ？分为活动的方块和停止的方块 ...
